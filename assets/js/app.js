895a1d23f3668ffbb1752cc7fab467d8ff6e8ebe
Blashford <ashford.blake@gmail.com>
2021-01-22 15:40:41 -0800

    added svg area

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
 // @TODO: YOUR CODE HERE!
+
+d3.csv("/assets/data/data.csv").then(data => {
+    console.log(data)
+
+var svgWidth = 1000;
+var svgHeight = 500;
+
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width);
+
+
+})
