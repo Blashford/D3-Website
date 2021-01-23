0e8e4ae1faea9873dfd31a09839bf549ed7b4aed
Blashford <ashford.blake@gmail.com>
2021-01-22 16:52:29 -0800

    got the axes to show up, working on the circles

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,9 @@ var svgWidth = 1000;
 var svgHeight = 500;
 
 var margin = {
-  top: 60,
+  top: 50,
   right: 60,
-  bottom: 60,
+  bottom: 100,
   left: 60
 };
 
@@ -16,8 +16,8 @@ var height = svgHeight - margin.top - margin.bottom;
 
 var svg = d3.select("#scatter")
     .append("svg")
-    .attr("height", height)
-    .attr("width", width);
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
 
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
@@ -33,6 +33,34 @@ d3.csv("/assets/data/data.csv").then(data => {
         d.poverty = +d.poverty;
         d.smokes = +d.smokes;
     })
+
+    var xLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.poverty)])
+        .range([0, width]);
     
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.healthcare)])
+        .range([height, 0])
+    
+    var xAxis = d3.axisBottom(xLinearScale);
+    var yAxis = d3.axisLeft(yLinearScale);
+    
+    // append axes
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+    
+    chartGroup.append("g")
+        .call(yAxis);
+    
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d=> d.poverty)
+        .attr("cy", d=> d.healthcare)
+        .attr("r", "10")
+        .attr("fill", "light blue")
+        .attr("text", d=> d.abbr)
 
 })
